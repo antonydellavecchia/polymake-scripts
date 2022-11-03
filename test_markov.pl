3e5cb3a59d86b70a8611073f3e0e743fdbe487b3
Antony Della Vecchia <vecchia@priort.math.tu-berlin.de>
2022-11-03 16:59:40 +0100

    initial commit

---
+++ test_markov.pl
@@ -0,0 +1,29 @@
+use application "fulton";
+
+
+sub run_markov {
+  # read matrix file in 4ti2 matrix format
+  my $matrix_file = $ARGV[0];
+  open(INPUT, "< ${matrix_file}");
+  my $first_line = true;
+  my @rows = ();
+  
+  while(<INPUT>) {
+    if ($first_line) {
+      $first_line = false;
+    } else {
+      push(@rows, $_);
+    }
+  }
+  close(INPUT);
+  
+  my $lattice = new Matrix<Integer>(@rows);
+  my $initial_time = time();
+
+  markov_basis($lattice);
+
+  print  "polymake took : ", time() - $initial_time, " \n";
+}
+
+run_markov();
+
